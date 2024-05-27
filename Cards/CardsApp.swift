a24a9f6a129b06ce906cae2e048443a372dc264c
narb <narborough.grahame@icloud.com>
2024-05-27 15:48:39 +0930

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Grahame Narborough on 27/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
